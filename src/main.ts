7d0731f83fe229a1e9c106d2b47dc17ae891d4e1
Irving Salazar <ISCIrvingDev@gmail.com>
2025-02-27 13:04:14 -0600

    Serve static files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'uploaded-files'), {
+    prefix: '/public/',
+  });
+  /*
+   * Archivos
+   ** http://localhost:3000/public/712893d25c2393bee67ceef49a359b9d
+   */
+
   await app.listen(3000);
 }
 bootstrap();
